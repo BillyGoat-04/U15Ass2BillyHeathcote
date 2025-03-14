d31d680346cc9666547b357d1ea4de09922c101e
BillyGoat-04 <Billyheathcote@hotmail.com>
2025-03-14 15:09:35 +0000

    Update javascript.js

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,1 +1,11 @@
+function validateForm() {
+  var name = document.getElementById('name').value;
+  var name = document.getElementById('email').value
+  var name = document.getElementById('pw').value
+  var name = document.getElementById('conPw').value
+  var errorMessage = document.getElementById('errorMessage');
 
+// Clear previous error messages
+errorMessage.innerHTML = 'Passwords do not mmatch';
+  return false;
+}
